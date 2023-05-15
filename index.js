32a5ee33ac9358a7d0a84279884c04a68ef22b17
Ferolinder <120025445+Ferolinder@users.noreply.github.com>
2023-05-15 16:43:25 +0200

    return to index logo

---
+++ index.js
@@ -0,0 +1,18 @@
+function main(){
+    console.log("prout");
+
+
+}
+
+function OnclickColorChange(id){
+    console.log("on a cliqué sur ",id);
+    let iidd = document.getElementById(id);
+    iidd.style.color='#fffffff';
+}
+
+function BackIndex(){
+    console.log("retour à l'acceuil");
+    
+}
+
+main();
