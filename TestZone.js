a296a61190ecd5663e8a8aaa6e5b0d2dcfdf8791
Ferolinder <120025445+Ferolinder@users.noreply.github.com>
2023-05-16 11:52:14 +0200

    qq changements dans la test Zone

--- TestZone.js
+++ TestZone.js
@@ -2,17 +2,20 @@
 function main(){
     console.log("prout");
     let acc = document.getElementsByTagName("p")[0];
-    acc.addEventListener('click',RedirectClick);
+    acc.addEventListener('click',() =>  {RedirectClick();
+                                        console.log("vroum vers acceuil");});
     
 }
 
 function RedirectClick(){
-    console.log("yes");
     setTimeout(function(){window.location.href="index.html"},2000);
-    let body = document.body;
-    body.style;
     
+    let body = document.body;
+    let LoaderZone = document.getElementById("loaderZone");
 
+    body.style.backgroundColor='#e9e3e3';
+    body.style.opacity="0.3";
+    LoaderZone.innerHTML='<span class="loader"></span>';    
 }
 
 main();
