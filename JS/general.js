527b2114eaa7194edaaf88fdfe64e5c330e7e8f7
Ferolinder <120025445+Ferolinder@users.noreply.github.com>
2023-05-17 14:55:40 +0200

    commit de cor

--- JS/general.js
+++ JS/general.js
@@ -1,5 +1,11 @@
 //ici on met toutes le fonctions générales à incorporer (tout ce qui concerne la navbar, le hooter ou tout ce que l'ont peut réutiliser);
 function main(){
+
+    var changePOsitionBox = document.getElementsByTagName("title")[0].baseURI;
+    console.log(changePOsitionBox);
+
+
+
     console.log("prout");
     var acc = document.getElementsByTagName("a")[0];
     var the = document.getElementsByTagName("a")[1];
@@ -7,25 +13,20 @@ function main(){
     var mem = document.getElementsByTagName("a")[3];
     var pro = document.getElementsByTagName("a")[4];
     var pub = document.getElementsByTagName("a")[5];
-    var con = document.getElementsByTagName("a")[6];
-    var pln = document.getElementsByTagName("a")[7];
-    var off = document.getElementsByTagName("a")[8];
+
+    var con = document.getElementsByTagName("a")[7];
+    var pln = document.getElementsByTagName("a")[8];
+    var off = document.getElementsByTagName("a")[9];
 
     acc.addEventListener("click",()=>RedirectClick("index.html"));
     the.addEventListener("click",()=>RedirectClick("thematiqueDeRecherche.html"));
     pla.addEventListener("click",()=>RedirectClick("plateformeTechnologique.html"));
     mem.addEventListener("click",()=>MembreClick());
-    pro.addEventListener("click",()=>RedirectClick("projet.html"));
+    pro.addEventListener("click",()=>RedirectClick("projets.html"));
     pub.addEventListener("click",()=>RedirectClick("publications.html"));
     con.addEventListener("click",()=>RedirectClick("contact.html"));
     pln.addEventListener("click",()=>RedirectClick("planAcces.html"));
-    off.addEventListener("click",()=>RedirectClick("offresDEmploi.html"));
-
-
-    let all = document.getElementsByTagName("a");
-    console.log(all);
-
-    
+    off.addEventListener("click",()=>RedirectClick("offresDEmploi.html"));  
     
 }
 
@@ -37,7 +38,8 @@ function RedirectClick(acces){
 
     body.style.backgroundColor='#e9e3e3';
     body.style.opacity="0.3";
-    LoaderZone.innerHTML='<span class="loader"></span>';    
+    LoaderZone.innerHTML='<span class="loader"></span>'; 
+    console.log("on tente d'aller vers ",acces,"ALERTE !!!! Vite, un loader le temps que l'ont finisse") 
 }
 
 function MembreClick(){
