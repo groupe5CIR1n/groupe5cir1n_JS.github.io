2a912d3ff7d4fee34ecb55aea9cd8f29d3fb3bd4
Ferolinder <120025445+Ferolinder@users.noreply.github.com>
2023-05-25 17:50:06 +0200

    quand on clique sur l'image, l'image change T w T !!!

--- JS/projets.js
+++ JS/projets.js
@@ -13,3 +13,16 @@ function toTheTop() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+function changeImage(article){
+  var pos = document.getElementsByTagName("img")[article];
+  console.log("VITE !! faut changer d'image sinon on vas penser que notre site est pas fini T w T");
+  if(pos.src=="http://127.0.0.1:5500/images/projets/transformationDigitale.png" || pos.src=="http://127.0.0.1:5500/images/projets/droneSousMarins.png" || pos.src=="http://127.0.0.1:5500/images/projets/phoques.png" || pos.src=="http://127.0.0.1:5500/images/projets/architectureElectronique.png"){
+    let resu = pos.src.slice(0, pos.src.length - 4);
+    pos.src = resu+"2"+".png";
+  }
+  else{
+    let resu = pos.src.slice(0, pos.src.length - 5);
+    pos.src = resu+".png";
+  }
+}
