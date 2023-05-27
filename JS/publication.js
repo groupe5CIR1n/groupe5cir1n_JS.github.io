1f8231f3e6d1b5b371956f97fef833ea0156781e
Ferolinder <120025445+Ferolinder@users.noreply.github.com>
2023-05-27 17:43:22 +0200

    organisation des publications

--- JS/publication.js
+++ JS/publication.js
@@ -1,12 +1,42 @@
 function main(){
     console.log("HAAAA Vite faire qqch !");
 
-    var colo = document.querySelector('select');
+    var year = document.querySelector('#selectedYear');
+    var colo = document.querySelector('#selectedColor');
+
+    year.addEventListener('change',function (){
+        let Year=["","#2022","#2023"];
+        if(this.selectedIndex==1){
+            var unset = document.getElementsByClassName("2023");
+            for(let i =0; i<unset.length;i++){
+                unset[i].style.display="none";
+            }
+        }
+        else{
+            var unset = document.getElementsByClassName("2023");
+            for(let i =0; i<unset.length;i++){
+                unset[i].style.display="block";
+            }
+        }
+        if(this.selectedIndex==2){
+            var unset = document.getElementsByClassName("2022");
+            for(let i =0; i<unset.length;i++){
+                unset[i].style.display="none";
+            }
+        }
+        else{
+            var unset = document.getElementsByClassName("2022");
+            for(let i =0; i<unset.length;i++){
+                unset[i].style.display="block";
+            }
+        }
+    })
+
+    
     colo.addEventListener('change', function () {
-        console.log('selectedIndex => '+this.selectedIndex);
         let colo=["","#c32b72","#196ca3"];
         if(this.selectedIndex==1){
-            var unset = document.getElementsByClassName("196ca3");
+            var unset = document.getElementsByClassName(colo[this.selectedIndex]);
             for(let i =0; i<unset.length;i++){
                 unset[i].style.display="none";
             }
@@ -30,7 +60,7 @@ function main(){
             }
         }
         document.body.style.backgroundColor=colo[this.selectedIndex];
-    })
+    });
 
 }
 
